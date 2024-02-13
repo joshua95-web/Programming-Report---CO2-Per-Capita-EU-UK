28cb6dec3c74e86bb17d719f7ca51c63b039296c
joshua95-web <joshcscurfield@gmail.com>
2024-02-13 16:48:00 +0000

    Initial commit

---
+++ index.py
@@ -0,0 +1,12 @@
+with open("CO2_Dataset.csv") as dataset:
+    CO2Data = dataset.read()
+
+print(CO2Data)
+
+# filter out EU countries with for loop
+# filter out UK countries with for loop
+# catalogue these in a new file
+
+# use pandas to group data into useful categories
+# use matplotlib to plot data
+
