2ccb485d527fddbce8366bc0baaa33bdd5ffb483
joshua95-web <joshcscurfield@gmail.com>
2024-03-12 15:59:49 +0000

    reformatted comments

--- index.py
+++ index.py
@@ -1,17 +1,18 @@
+
+
+
+# filter out EU countries as per list of EU countries (UK Government, 2014)
+#%%
 import pandas as pd
 
 df = pd.read_csv('CO2_Dataset.csv')
 
-
-# filter out EU countries as per list of EU countries (UK Government, 2014)
-
-
 eu_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
 eu_df = df[df['Country Name'].isin(eu_countries)]
 eu_df.to_csv('eu_countries.csv')
+#%%
+import pandas as pd
 
-# filter out UK countries with for loop
-# catalogue these in a new file
 
 # use pandas to group data into useful categories
 # use matplotlib to plot data
