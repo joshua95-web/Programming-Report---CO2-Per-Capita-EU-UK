b2655ef51ba5a9c17a368629c90e2a88eba92af9
joshua95-web <joshcscurfield@gmail.com>
2024-03-13 14:07:53 +0000

    changed function to grab UK as well as EU from CSV

--- index.py
+++ index.py
@@ -1,19 +1,21 @@
 
 
 
-# filter out EU countries as per list of EU countries (UK Government, 2014)
 #%%
 import pandas as pd
+
+
 
 df = pd.read_csv('CO2_Dataset.csv')
 
-eu_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
-eu_df = df[df['Country Name'].isin(eu_countries)]
-eu_df.to_csv('eu_countries.csv')
+# filter out EU countries as per list of EU countries (UK Government, 2014) and United Kingdom
+
+eu_and_UK_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'United Kingdom']
+eu_UK_df = df[df['Country Name'].isin(eu_and_UK_countries)]
+eu_UK_df.to_csv('eu_and_UK_countries.csv')
 #%%
 
-# Find CSV of UK countries CO2 emissions per capita
-import pandas as pd
+
 
 
 
